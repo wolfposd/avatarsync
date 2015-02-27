e3d495a6a8327207acb08a3214b783d5d74d289b
Wolf Posdorfer <9posdorf@informatik.uni-hamburg.de>
2015-02-27 12:53:08 +0100

    adding license

--- AvatarSync/AvatarSyncTests/AvatarSyncTests.m
+++ AvatarSync/AvatarSyncTests/AvatarSyncTests.m
@@ -1,9 +1,20 @@
 //
-//  AvatarSyncTests.m
-//  AvatarSyncTests
+// AvatarSync
+// Copyright (C) 2014-2015  Wolf Posdorfer
 //
-//  Created by Wolf Posdorfer on 23.11.14.
-//  Copyright (c) 2014 Wolf Posdorfer. All rights reserved.
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 //
 
 #import <UIKit/UIKit.h>
@@ -17,16 +28,13 @@
 
 - (void)setUp {
     [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
 }
 
 - (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
     [super tearDown];
 }
 
 - (void)testExample {
-    // This is an example of a functional test case.
     XCTAssert(YES, @"Pass");
 }
 
